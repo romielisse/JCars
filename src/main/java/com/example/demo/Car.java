5d48f427fb92094d8dff9cda8eca83a190ab2fc1
jihyunle <jennifer.jlee@outlook.com>
2019-03-08 09:43:36 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,84 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 3)
+    private String type;
+
+    @NotNull
+    @Size(min = 3)
+    private String make;
+
+    @NotNull
+    @Size(min = 1)
+    private String model;
+
+    @NotNull
+    @Size(min = 4)
+    private int year;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    public Category category;
+
+    public Car(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
